2f5d864f70396ee0d36013facb71e28a9bb16964
Sharif Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-05-21 15:48:55 +0600

    - Updated index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,29 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>Document</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Work with Documents in Allors
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>Excel</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Work with Excel in Allors
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
+    title: <>Dynamic</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Allors DLR Implementation
       </>
     ),
   },
