b5ab3bf5fe0a52216ffbad0313a2bfadea706789
jubayerarefin <jubayerarefin@users.noreply.github.com>
2020-08-26 16:23:48 +0000

    Deploy website - based on b3697c0cc6b3490569f2c20c2d041279709fa0da

---
+++ 967e9999.190b10b4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{77:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"allors","name":"allors","count":2,"permalink":"/blog/tags/allors"}')}}]);
---
+++ a88008ee.a6037f36.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{80:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"update","name":"update","count":2,"permalink":"/blog/tags/update"}')}}]);
