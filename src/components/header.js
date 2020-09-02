976a500ad7f500be8c7e9c2dbfa148aabcdf19a6
Sharif Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2020-09-02 19:22:15 +0600

    Updates header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,23 @@
 import React from "react";
 
+const txtAlign = "center";
+const txtPadding = "30px";
+const borderRadius = "10px";
+const bgColor = "#25c2a0";
+
 export class Header extends React.Component {
   constructor(props) {
     super(props);
-
-    this.txtAlign= "center";
-    this.txtPadding= "30px";
-    this.borderRadius= "10px";
-    this.bgColor= "#25c2a0";
   }
 
   render() {
     return (
       <div
         style={{
-          textAlign: this.txtAlign,
-          padding: this.txtPadding,
-          backgroundColor: this.bgColor,
-          borderRadius: this.borderRadius,
+          textAlign: this.props.txtAlign ?? txtAlign,
+          padding: this.props.txtPadding ?? txtPadding,
+          backgroundColor: this.props.bgColor ?? bgColor,
+          borderRadius: this.props.borderRadius ?? borderRadius,
         }}
       >
         <h1>{this.props.title}</h1>
